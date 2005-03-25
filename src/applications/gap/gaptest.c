8d647e33fcee16045d4573bf99c36869aea85d16
grothoff <grothoff@140774ce-b5e7-0310-ab8b-a85725594a96>
2005-03-25 22:16:17 +0000

    towards gaptest
    git-svn-id: https://gnunet.org/svn/GNUnet@486 140774ce-b5e7-0310-ab8b-a85725594a96

---
+++ src/applications/gap/gaptest.c
@@ -0,0 +1,206 @@
+/*
+     This file is part of GNUnet.
+     (C) 2005 Christian Grothoff (and other contributing authors)
+
+     GNUnet is free software; you can redistribute it and/or modify
+     it under the terms of the GNU General Public License as published
+     by the Free Software Foundation; either version 2, or (at your
+     option) any later version.
+
+     GNUnet is distributed in the hope that it will be useful, but
+     WITHOUT ANY WARRANTY; without even the implied warranty of
+     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+     General Public License for more details.
+
+     You should have received a copy of the GNU General Public License
+     along with GNUnet; see the file COPYING.  If not, write to the
+     Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+     Boston, MA 02111-1307, USA.
+*/
+
+/**
+ * @file applications/session/sessiontest.c
+ * @brief Session establishment testcase
+ * @author Christian Grothoff
+ */
+
+#include "platform.h"
+#include "gnunet_protocols.h"
+#include "gnunet_stats_lib.h"
+#include <sys/wait.h>
+
+static int parseOptions(int argc,
+			char ** argv) {
+  FREENONNULL(setConfigurationString("GNUNETD",
+				     "LOGFILE",
+				     NULL));
+  return OK;
+}
+
+/**
+ * Identity of peer 2 (hardwired).
+ */
+static PeerIdentity peer2;
+
+static int waitForConnect(const char * name,
+			  unsigned long long value,
+			  void * cls) {
+  if ( (value > 0) &&
+       (0 == strcmp(_("# of connected peers"),
+		    name)) )
+    return SYSERR;
+  return OK;
+}
+
+/**
+ * Testcase to test p2p session key exchange.
+ *
+ * @param argc number of arguments from the command line
+ * @param argv command line arguments
+ * @return 0: ok, -1: error
+ */
+int main(int argc, char ** argv) {
+  pid_t daemon1;
+  pid_t daemon2;
+  int ret;
+  int status;
+  GNUNET_TCP_SOCKET * sock;
+  int left;
+
+  GNUNET_ASSERT(OK ==
+		enc2hash("BV3AS3KMIIBVIFCGEG907N6NTDTH26B7T6FODUSLSGK"
+			 "5B2Q58IEU1VF5FTR838449CSHVBOAHLDVQAOA33O77F"
+			 "OPDA8F1VIKESLSNBO",
+			 &peer2.hashPubKey));
+  daemon1 = fork();
+  if (daemon1 == 0) {
+    if (0 != execlp("gnunetd", /* what binary to execute, must be in $PATH! */
+		    "gnunetd", /* arg0, path to gnunet binary */
+		    "-d",  /* do not daemonize so we can easily kill you */
+		    "-c",
+		    "peer1.conf", /* configuration file */
+		    NULL)) {
+      fprintf(stderr,
+	      _("'%s' failed: %s\n"),
+	      "execlp",
+	      STRERROR(errno));
+      return -1;
+    }
+  }
+  daemon2 = fork();
+  if (daemon2 == 0) {
+    if (0 != execlp("gnunetd", /* what binary to execute, must be in $PATH! */
+		    "gnunetd", /* arg0, path to gnunet binary */
+		    "-d",  /* do not daemonize so we can easily kill you */
+		    "-c",
+		    "peer2.conf", /* configuration file */
+		    NULL)) {
+      fprintf(stderr,
+	      _("'%s' failed: %s\n"),
+	      "execlp",
+	      STRERROR(errno));
+      return -1;
+    }
+  }
+  /* in case existing HELOs have expired */
+  sleep(5);
+  system("cp peer1/data/hosts/* peer2/data/hosts/");
+  system("cp peer2/data/hosts/* peer1/data/hosts/");
+  if (daemon1 != -1) {
+    if (0 != kill(daemon1, SIGTERM))
+      DIE_STRERROR("kill");
+    if (daemon1 != waitpid(daemon1, &status, 0))
+      DIE_STRERROR("waitpid");
+  }
+  if (daemon2 != -1) {
+    if (0 != kill(daemon2, SIGTERM))
+      DIE_STRERROR("kill");
+    if (daemon2 != waitpid(daemon2, &status, 0))
+      DIE_STRERROR("waitpid");
+  }
+
+  /* re-start, this time we're sure up-to-date HELOs are available */
+  daemon1 = fork();
+  if (daemon1 == 0) {
+    if (0 != execlp("gnunetd", /* what binary to execute, must be in $PATH! */
+		    "gnunetd", /* arg0, path to gnunet binary */
+		    "-d",  /* do not daemonize so we can easily kill you */
+		    "-c",
+		    "peer1.conf", /* configuration file */
+		    NULL)) {
+      fprintf(stderr,
+	      _("'%s' failed: %s\n"),
+	      "execlp",
+	      STRERROR(errno));
+      return -1;
+    }
+  }
+  daemon2 = fork();
+  if (daemon2 == 0) {
+    if (0 != execlp("gnunetd", /* what binary to execute, must be in $PATH! */
+		    "gnunetd", /* arg0, path to gnunet binary */
+		    "-d",  /* do not daemonize so we can easily kill you */
+		    "-c",
+		    "peer2.conf", /* configuration file */
+		    NULL)) {
+      fprintf(stderr,
+	      _("'%s' failed: %s\n"),
+	      "execlp",
+	      STRERROR(errno));
+      return -1;
+    }
+  }
+  sleep(5);
+
+
+  ret = 0;
+  left = 5;
+  /* wait for connection or abort with error */
+  initUtil(argc, argv, &parseOptions);
+  do {
+    sock = getClientSocket();
+    if (sock == NULL) {
+      printf(_("Waiting for gnunetd to start (%u iterations left)...\n"),
+	     left);
+      sleep(1);
+      left--;
+      if (left == 0) {
+	ret = 1;
+	break;
+      }
+    }
+  } while (sock == NULL);
+
+  left = 30; /* how many iterations should we wait? */
+  while (OK == requestStatistics(sock,
+				 &waitForConnect,
+				 NULL)) {
+    printf(_("Waiting for peers to connect (%u iterations left)...\n"),
+	   left);
+    sleep(5);
+    left--;
+    if (left == 0) {
+      ret = 1;
+      break;
+    }
+  }
+  releaseClientSocket(sock);
+  doneUtil();
+
+  /* also shutdown daemons again */
+  if (daemon1 != -1) {
+    if (0 != kill(daemon1, SIGTERM))
+      DIE_STRERROR("kill");
+    if (daemon1 != waitpid(daemon1, &status, 0))
+      DIE_STRERROR("waitpid");
+  }
+  if (daemon2 != -1) {
+    if (0 != kill(daemon2, SIGTERM))
+      DIE_STRERROR("kill");
+    if (daemon2 != waitpid(daemon2, &status, 0))
+      DIE_STRERROR("waitpid");
+  }
+  return ret;
+}
+
+/* end of sessiontest.c */
