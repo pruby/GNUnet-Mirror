5ea9ed4f17ccf22165128b3562e0d8d114040cfb
durner <durner@140774ce-b5e7-0310-ab8b-a85725594a96>
2005-06-20 20:34:04 +0000

    new PlibC
    git-svn-id: https://gnunet.org/svn/GNUnet@965 140774ce-b5e7-0310-ab8b-a85725594a96

--- src/include/plibc.h
+++ src/include/plibc.h
@@ -22,7 +22,7 @@
  * @brief PlibC header
  * @attention This file is usually not installed under Unix,
  *            so ship it with your application
- * @version $Revision: 1.17 $
+ * @version $Revision: 1.19 $
  */
 
 #ifndef _PLIBC_H_
@@ -68,48 +68,6 @@ extern "C" {
 #define int32_t long
 
 /* Thanks to the Cygwin project */
-#define EPERM 1		/* Not super-user */
-#define ENOENT 2	/* No such file or directory */
-#define ESRCH 3		/* No such process */
-#define EINTR 4		/* Interrupted system call */
-#define EIO 5		/* I/O error */
-#define ENXIO 6		/* No such device or address */
-#define E2BIG 7		/* Arg list too long */
-#define ENOEXEC 8	/* Exec format error */
-#define EBADF 9		/* Bad file number */
-#define ECHILD 10	/* No children */
-#define EAGAIN 11	/* Resource unavailable or operation would block, try again */
-#define ENOMEM 12	/* Not enough memory */
-#define EACCES 13	/* Permission denied */
-#define EFAULT 14	/* Bad address */
-#define ENOTBLK 15	/* Block device required */
-#define EBUSY 16	/* Mount device busy */
-#define EEXIST 17	/* File exists */
-#define EXDEV 18	/* Cross-device link */
-#define ENODEV 19	/* No such device */
-#define ENOTDIR 20	/* Not a directory */
-#define EISDIR 21	/* Is a directory */
-#define EINVAL 22	/* Invalid argument */
-#define ENFILE 23	/* Too many open files in system */
-#define EMFILE 24	/* Too many open files */
-#define ENOTTY 25	/* Not a typewriter */
-#define ETXTBSY 26	/* Text file busy */
-#define EFBIG 27	/* File too large */
-#define ENOSPC 28	/* No space left on device */
-#define ESPIPE 29	/* Illegal seek */
-#define EROFS 30	/* Read only file system */
-#define EMLINK 31	/* Too many links */
-#define EPIPE 32	/* Broken pipe */
-#define EDOM 33		/* Math arg out of domain of func */
-#define ERANGE 34	/* Math result not representable */
-#define ENOMSG 35	/* No message of desired type */
-#define EIDRM 36	/* Identifier removed */
-#define ECHRNG 37	/* Channel number out of range */
-#define EL2NSYNC 38	/* Level 2 not synchronized */
-#define L3HLT 39	/* Level 3 halted */
-#define EL3RST 40	/* Level 3 reset */
-#define ELNRNG 41	/* Link number out of range */
-#define EUNATCH 42	/* Protocol driver not attached */
 #define ENOCSI 43	/* No CSI structure available */
 #define EL2HLT 44	/* Level 2 halted */
 #ifndef  EDEADLK
@@ -125,7 +83,7 @@ extern "C" {
 #define EBADRQC 54	/* Invalid request code */
 #define EBADSLT 55	/* Invalid slot */
 #ifndef  EDEADLOCK
-	#define EDEADLOCK 56	/* File locking deadlock error */
+	#define EDEADLOCK EDEADLK	/* File locking deadlock error */
 #endif
 #define EBFONT 57	/* Bad font file fmt */
 #define ENOSTR 60	/* Device not a stream */
@@ -319,6 +277,7 @@ typedef struct
 
 BOOL _plibc_CreateShortcut(const char *pszSrc, const char *pszDest);
 BOOL _plibc_DereferenceShortcut(char *pszShortcut);
+char *plibc_ChooseDir(char *pszTitle, unsigned long ulFlags);
 long QueryRegistry(HKEY hMainKey, char *pszKey, char *pszSubKey,
               char *pszBuffer, long *pdLength);
 
