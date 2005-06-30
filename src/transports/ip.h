8a552990c0b985a413f39afed9fd6141502aad38
grothoff <grothoff@140774ce-b5e7-0310-ab8b-a85725594a96>
2005-06-30 12:30:42 +0000

    oooop
    git-svn-id: https://gnunet.org/svn/GNUnet@1159 140774ce-b5e7-0310-ab8b-a85725594a96

---
+++ src/transports/ip.h
@@ -0,0 +1,54 @@
+/*
+     This file is part of GNUnet.
+     (C) 2001, 2002, 2003, 2004, 2005 Christian Grothoff (and other contributing authors)
+
+     GNUnet is free software; you can redistribute it and/or modify
+     it under the terms of the GNU General Public License as published
+     by the Free Software Foundation; either version 2, or (at your
+     option) any later version.
+
+     GNUnet is distributed in the hope that it will be useful, but
+     WITHOUT ANY WARRANTY; without even the implied warranty of
+     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+     General Public License for more details.
+
+     You should have received a copy of the GNU General Public License
+     along with GNUnet; see the file COPYING.  If not, write to the
+     Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+     Boston, MA 02111-1307, USA.
+*/
+
+/**
+ * @file transports/ip.h
+ * @brief
+ *
+ * @author Christian Grothoff
+ */
+
+#ifndef IP_H
+#define IP_H
+
+/**
+ * @brief Determine the (external) IP of the local machine.
+ *
+ * We have many ways to get that IP:
+ * a) from the interface (ifconfig)
+ * b) via DNS from our HOSTNAME (environment)
+ * c) from the configuration (HOSTNAME specification or static IP)
+ *
+ * Which way applies depends on the OS, the configuration
+ * (dynDNS? static IP? NAT?) and at the end what the user
+ * needs.
+ *
+ * @return SYSERR on error, OK on success
+ */
+int getPublicIPAddress(IPaddr  * address);
+
+/**
+ * @brief Get the IPv6 address for the local machine.
+ *
+ * @return SYSERR on error, OK on success
+ */
+int getPublicIP6Address(IP6addr  * address);
+
+#endif
