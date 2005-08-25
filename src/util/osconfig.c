55fb889018ceca290abd5c2d38a8162f4f6a9162
grothoff <grothoff@140774ce-b5e7-0310-ab8b-a85725594a96>
2005-08-25 05:19:08 +0000

    mantis 899
    git-svn-id: https://gnunet.org/svn/GNUnet@1898 140774ce-b5e7-0310-ab8b-a85725594a96

--- src/util/osconfig.c
+++ src/util/osconfig.c
@@ -223,7 +223,7 @@ int autostartService(int doAutoStart, char *username, char *groupname) {
 	      "# Automatically created by gnunet-setup\n"
 	      "#\n"
 	      "\n"
-	      "PATH=$PATH:" PREFIX_PATH 
+	      "PATH=$PATH:" PREFIX_PATH "/bin"
 	      "PIDFILE=/var/run/gnunetd/gnunetd.pid\n"
 	      "\n"
 	      "case \"$1\" in\n"
