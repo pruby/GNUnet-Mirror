33529f7c1687d1215accd6faa5755265a270f850
durner <durner@140774ce-b5e7-0310-ab8b-a85725594a96>
2005-12-18 20:43:04 +0000

    typo
    git-svn-id: https://gnunet.org/svn/GNUnet@2344 140774ce-b5e7-0310-ab8b-a85725594a96

--- src/conf/mconf.c
+++ src/conf/mconf.c
@@ -539,7 +539,7 @@ static void conf(struct menu *menu)
 			break;
 		if (menu == &rootmenu) {
 			cmake(); cset_tag(':', NULL); cprint_name("--- ");
-			cmake(); cset_tag('L', NULL); cprint_name("Load an Altenatie Configuration File");
+			cmake(); cset_tag('L', NULL); cprint_name("Load an Alternate Configuration File");
 		}
 		dialog_clear();
 		/* active_item itself can change after any creset() +
