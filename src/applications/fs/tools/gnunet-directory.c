7dea50393c8bf86dfa3920f94dfb1ede7ddd5f1b
grothoff <grothoff@140774ce-b5e7-0310-ab8b-a85725594a96>
2006-03-12 04:55:14 +0000

    fix
    git-svn-id: https://gnunet.org/svn/GNUnet@2490 140774ce-b5e7-0310-ab8b-a85725594a96

--- src/applications/fs/tools/gnunet-directory.c
+++ src/applications/fs/tools/gnunet-directory.c
@@ -88,19 +88,29 @@ static void printDirectory(const char * filename) {
   fd = fileopen(name,
 		O_RDONLY);
 #endif
-  data = MMAP(NULL,
-	      len,
-	      PROT_READ,
-	      MAP_SHARED,
-	      fd,
-	      0);
-  ret = ECRS_listDirectory(data,
-			   len,
-			   &md,
-			   &printNode,
-			   NULL);
-  MUNMAP(data, len);
-  closefile(fd);
+  if (fd == -1) {
+    LOG_FILE_STRERROR(LOG_ERROR, "open", name);
+    ret = -1;
+  } else {
+    data = MMAP(NULL,
+		len,
+		PROT_READ,
+		MAP_SHARED,
+		fd,
+		0);
+    if (data == MAP_FAILED) {
+      LOG_FILE_STRERROR(LOG_ERROR, "mmap", name);
+      ret = -1;
+    } else {
+      ret = ECRS_listDirectory(data,
+			       len,
+			       &md,
+			       &printNode,
+			       NULL);
+      MUNMAP(data, len);
+    }
+    closefile(fd);
+  }
   if (ret == -1)
     printf(_("File format error (not a GNUnet directory?)\n"));
   else
