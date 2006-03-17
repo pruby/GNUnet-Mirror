905257cb2f51ebd2d5914acf8b043afbffece316
durner <durner@140774ce-b5e7-0310-ab8b-a85725594a96>
2006-03-17 19:29:35 +0000

    print initialization errors
    git-svn-id: https://gnunet.org/svn/GNUnet@2543 140774ce-b5e7-0310-ab8b-a85725594a96

--- src/util/win/winproc.c
+++ src/util/win/winproc.c
@@ -1,6 +1,6 @@
 /*
      This file is part of GNUnet.
-     (C) 2001, 2002, 2003, 2004, 2005 Christian Grothoff (and other contributing authors)
+     (C) 2001, 2002, 2003, 2004, 2005, 2006 Christian Grothoff (and other contributing authors)
 
      GNUnet is free software; you can redistribute it and/or modify
      it under the terms of the GNU General Public License as published
@@ -80,8 +80,8 @@ int InitWinEnv()
 {
 	int ret;
 	
+  plibc_set_panic_proc(plibc_panic);
 	ret = plibc_init("GNU", PACKAGE);
-	plibc_set_panic_proc(plibc_panic);
 
   hNTDLL = LoadLibrary("ntdll.dll");
 
