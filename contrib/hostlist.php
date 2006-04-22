aae8bd6ae9b3a625846056205664b3654c4b5ef9
grothoff <grothoff@140774ce-b5e7-0310-ab8b-a85725594a96>
2006-04-22 15:01:12 +0000

    syn
    git-svn-id: https://gnunet.org/svn/GNUnet@2625 140774ce-b5e7-0310-ab8b-a85725594a96

---
+++ contrib/hostlist.php
@@ -0,0 +1,24 @@
+<?php
+// Requires PHP >= 4.3.0
+// Author: "Krasko Oleksandr" <0m3r.mail@gmail.com>
+// Minor improvements by Christian Grothoff <christian@grothoff.org>
+header("Content-Type: application/octet-stream\r\n\r\n");
+$extmas = array('6','8','12','17','23','25');
+$path = '/var/lib/GNUnet/data/hosts/'; // adjust as necessary
+$dir = opendir($path);
+if (! $dir)
+  die("Cannot open directory $path.\n");
+$mas = array();
+while ($fname = readdir($dir)) {
+  if (is_file($path . '/' . $fname)) {
+    $dpo = strpos($fname, '.') + 1;
+    $len = strlen($fname);
+    if (in_array(substr($fname, $dpo - $len, $extmas)) {
+      $mas[] = $fname;
+    }
+  }
+}
+shuffle($mas); // randomize order
+foreach ($mas as $val) 
+  echo file_get_contents($val);
+?>
