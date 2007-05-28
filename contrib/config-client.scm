f3ed6c0e8a3a806b68a35154457b6da2e7698c72
grothoff <grothoff@140774ce-b5e7-0310-ab8b-a85725594a96>
2007-05-28 18:55:21 +0000

    expr
    git-svn-id: https://gnunet.org/svn/GNUnet@4828 140774ce-b5e7-0310-ab8b-a85725594a96

--- contrib/config-client.scm
+++ contrib/config-client.scm
@@ -258,8 +258,8 @@ The default is to use filenames and to break larger words at spaces (and undersc
   (_ "GNUnet uses two bytes per entry on the disk.  This database is used to keep track of how a particular URI has been used in the past.  For example, GNUnet may remember that a particular URI has been found in a search previously or corresponds to a file uploaded by the user.  This information can then be used by user-interfaces to filter URI lists, such as search results.  If the database is full, older entries will be discarded.  The default value should be sufficient without causing undue disk utilization." )
   '()
   #t
-  1024*1024
-  (cons 1 1024*1024*1024)
+  1048576
+  (cons 1 1073741824)
   'rare) )
 
 (define (fs builder)
