a701c4f8e81fb3ee196600c14c9ee118ddf4e5bb
grothoff <grothoff@140774ce-b5e7-0310-ab8b-a85725594a96>
2008-06-02 03:15:57 +0000

    bump
    git-svn-id: https://gnunet.org/svn/GNUnet@6998 140774ce-b5e7-0310-ab8b-a85725594a96

--- src/include/gnunet_util.h
+++ src/include/gnunet_util.h
@@ -85,7 +85,7 @@ extern "C"
  * especially the least significant bits may change
  * frequently, even between different SVN versions.
  */
-#define GNUNET_UTIL_VERSION 0x00080000
+#define GNUNET_UTIL_VERSION 0x00080001
 
 #if 0                           /* keep Emacsens' auto-indent happy */
 {
