140b1953018d849aa1fa7bc3a052ad0915474364
moon <moon@140774ce-b5e7-0310-ab8b-a85725594a96>
2008-06-14 14:16:29 +0000

    add more default hostlists for gnunet-setup
    git-svn-id: https://gnunet.org/svn/GNUnet@7208 140774ce-b5e7-0310-ab8b-a85725594a96

--- contrib/config-daemon.scm
+++ contrib/config-daemon.scm
@@ -187,8 +187,7 @@ If you want to setup an alternate hostlist server, you must run a permanent node
 If you do not specify a HOSTLISTURL, you must copy valid hostkeys to data/hosts manually.")
   '()
   #t
-;;  "http://gnunet.org/hostlist.php http://gnunet.mine.nu:8081/hostlist http://de.gnunet.org/cgi-bin/hostlist.cgi"
-  "http://vserver1236.vserver-on.de/hostlist-074"
+  "http://gnunet.org/hostlist.php http://gnunet.mine.nu:8081/hostlist http://vserver1236.vserver-on.de/hostlist-074"
   '()
   'always) )
 
