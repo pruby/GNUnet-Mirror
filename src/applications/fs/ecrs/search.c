84aafaf0fe524203475e788a417a2d2096ca6f30
grothoff <grothoff@140774ce-b5e7-0310-ab8b-a85725594a96>
2008-06-29 05:42:34 +0000

    fixing namespace discovery
    git-svn-id: https://gnunet.org/svn/GNUnet@7361 140774ce-b5e7-0310-ab8b-a85725594a96

--- src/applications/fs/ecrs/search.c
+++ src/applications/fs/ecrs/search.c
@@ -229,7 +229,7 @@ process_sblock_result (const GNUNET_EC_SBlock * sb,
   static GNUNET_HashCode allZeros;
   struct GNUNET_GE_Context *ectx = sqc->ectx;
   GNUNET_ECRS_FileInfo fi;
-  struct GNUNET_ECRS_URI updateURI;
+  URI updateURI;
   int ret;
   const char *id;
   const char *uris;
@@ -253,11 +253,21 @@ process_sblock_result (const GNUNET_EC_SBlock * sb,
     }
   isRoot = 0 == memcmp (&sb->identifier, &allZeros, sizeof (GNUNET_HashCode));
   fi.uri = GNUNET_ECRS_string_to_uri (ectx, uris);
+  if ( (isRoot) &&
+       (fi.uri == NULL) )
+    {
+      fi.uri = GNUNET_malloc(sizeof(URI));
+      fi.uri->type = sks;
+      GNUNET_hash (&sb->subspace,
+		   sizeof(GNUNET_RSA_PublicKey),
+		   &fi.uri->data.sks.namespace);
+      fi.uri->data.sks.identifier = GNUNET_strdup(id);
+    }
   if (fi.uri == NULL)
     {
       GNUNET_GE_BREAK_OP (ectx, 0);     /* sblock malformed */
       GNUNET_meta_data_destroy (fi.meta);
-      return GNUNET_SYSERR;
+      return GNUNET_SYSERR;      
     }
   if (sqc->spcb != NULL)
     {
@@ -267,7 +277,8 @@ process_sblock_result (const GNUNET_EC_SBlock * sb,
     }
   else
     ret = GNUNET_OK;
-  if (strlen (id) > 0)
+  if ( (strlen (id) > 0) &&
+       (strlen (uris) > 0) )
     {
       updateURI.type = sks;
       GNUNET_hash (&sb->subspace,
@@ -278,6 +289,7 @@ process_sblock_result (const GNUNET_EC_SBlock * sb,
       GNUNET_free (updateURI.data.sks.identifier);
     }
   GNUNET_meta_data_destroy (fi.meta);
+  GNUNET_ECRS_uri_destroy (fi.uri);
   return ret;
 }
 
