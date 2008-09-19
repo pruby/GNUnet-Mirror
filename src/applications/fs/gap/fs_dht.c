0cd44b845b1e240e59c1b88267656663288088df
grothoff <grothoff@140774ce-b5e7-0310-ab8b-a85725594a96>
2008-09-19 06:13:04 +0000

    fixing #1426
    git-svn-id: https://gnunet.org/svn/GNUnet@7715 140774ce-b5e7-0310-ab8b-a85725594a96

--- src/applications/fs/gap/fs_dht.c
+++ src/applications/fs/gap/fs_dht.c
@@ -170,13 +170,17 @@ GNUNET_FS_DHT_execute_query (unsigned int type, const GNUNET_HashCode * query)
 {
   struct ActiveRequestRecords *record;
   GNUNET_CronTime now;
+  struct GNUNET_DHT_GetHandle * h;
 
   if (dht == NULL)
     return;
+  h = dht->get_start (type, query, &response_callback, record);
+  if (h == NULL)
+    return; /* failed in DHT */
   now = GNUNET_get_time ();
   record = GNUNET_malloc (sizeof (struct ActiveRequestRecords));
   record->end_time = now + GNUNET_GAP_MAX_DHT_DELAY;
-  record->handle = dht->get_start (type, query, &response_callback, record);
+  record->handle = h;
   record->type = type;
   GNUNET_mutex_lock (GNUNET_FS_lock);
   record->next = records;
