156a973a0724d39e4611db5df953cd25b444da22
grothoff <grothoff@140774ce-b5e7-0310-ab8b-a85725594a96>
2008-12-26 08:42:51 +0000

    fix
    git-svn-id: https://gnunet.org/svn/GNUnet@8019 140774ce-b5e7-0310-ab8b-a85725594a96

--- contrib/coverage.sh
+++ contrib/coverage.sh
@@ -2,6 +2,13 @@
 # make sure configure was run with coverage enabled...
 lcov --directory . --zerocounters
 make check
+for n in `find * -name "*.gcno" | grep libs`
+do
+  cd `dirname $n`
+  mv `basename $n` ..
+  cd -
+done
+rm -f src/util/libgnunetutil.gcda
 lcov --directory . --capture --output-file app.info
-mkdir /tmp/coverage
-genhtml -o /tmp/coverage app.info
+mkdir doc/coverage
+genhtml -o doc/coverage app.info
