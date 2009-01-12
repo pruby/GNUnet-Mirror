7d862e1855a3d4d6f65c52bbf82dfbacaf38558a
grothoff <grothoff@140774ce-b5e7-0310-ab8b-a85725594a96>
2009-01-12 20:02:52 +0000

    fix
    git-svn-id: https://gnunet.org/svn/GNUnet@8100 140774ce-b5e7-0310-ab8b-a85725594a96

--- src/applications/fs/gap/fs.c
+++ src/applications/fs/gap/fs.c
@@ -694,7 +694,7 @@ handle_p2p_query (const GNUNET_PeerIdentity * sender,
                                &req->queries[0],
                                ntohl (req->filter_mutator),
                                bloomfilter_size,
-                               &req->queries[query_count + 1]);
+                               &req->queries[query_count]);
   return GNUNET_OK;
 }
 
