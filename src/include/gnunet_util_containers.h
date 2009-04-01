a943e6b0cc7bf04c6392fcd862993aab0b64aee1
nevans <nevans@140774ce-b5e7-0310-ab8b-a85725594a96>
2009-04-01 01:08:08 +0000

    heap methods into util_containers.h
    git-svn-id: https://gnunet.org/svn/GNUnet@8368 140774ce-b5e7-0310-ab8b-a85725594a96

--- src/include/gnunet_util_containers.h
+++ src/include/gnunet_util_containers.h
@@ -578,7 +578,106 @@ void *GNUNET_multi_hash_map_get_random (const struct GNUNET_MultiHashMap
     (element)->next->prev = (element)->prev;
 
 
+typedef unsigned int GNUNET_CostType;
 
+/*
+ * Heap type, either max or min.  Hopefully makes the
+ * implementation more useful.
+ */
+typedef enum
+{
+  GNUNET_MAX_HEAP = 0,
+  GNUNET_MIN_HEAP = 1,
+} GNUNET_CONTAINER_HeapType;
+
+/*
+ * Struct that is stored in hashmap, pointers to
+ * locations in min_heap and max_heap.
+ */
+struct GNUNET_CONTAINER_Heap;
+
+struct GNUNET_CONTAINER_Heap
+  *GNUNET_CONTAINER_heap_create (GNUNET_CONTAINER_HeapType type);
+
+void GNUNET_CONTAINER_heap_destroy (struct GNUNET_CONTAINER_Heap *h);
+
+/**
+ * Iterator for heap
+ *
+ * @param value - obj stored in heap
+ * @param root - root of heap in which obj is stored
+ * @param cls - client arg passed through
+ * @return GNUNET_YES if we should continue to
+ *         iterate,
+ *         GNUNET_NO if not.
+ */
+typedef int (*GNUNET_CONTAINER_HeapIterator) (void *element,
+                                              GNUNET_CostType cost,
+                                              struct GNUNET_CONTAINER_Heap *
+                                              root, void *cls);
+
+/**
+ * Iterate over all entries in the map.
+ *
+ * @param heap - the heap
+ * @param iterator - function to call on each entry
+ * @param cls - client argument (closure)
+ * @return - number of items handled
+ *         GNUNET_SYSERR if there's a problem
+ */
+int GNUNET_CONTAINER_heap_iterate (struct GNUNET_CONTAINER_Heap *heap,
+                                   GNUNET_CONTAINER_HeapIterator iterator,
+                                   void *cls);
+
+
+/**
+ * Inserts a new item into the heap, item is always neighbor now.
+ */
+int
+GNUNET_CONTAINER_heap_insert (struct GNUNET_CONTAINER_Heap *root,
+                              void *element, GNUNET_CostType cost);
+
+/**
+ * Removes root of the tree, is remove max if a max heap and remove min
+ * if a min heap, returns the data stored at the node.
+ */
+void *GNUNET_CONTAINER_heap_remove_root (struct GNUNET_CONTAINER_Heap *root);
+
+/**
+ * Returns data stored at root of tree, doesn't effect anything
+ */
+void *GNUNET_CONTAINER_heap_peek (struct GNUNET_CONTAINER_Heap *root);
+
+/**
+ * Removes any node from the tree based on the neighbor given, does
+ * not traverse the tree (backpointers) but may take more time due to
+ * percolation of nodes.
+ */
+void *GNUNET_CONTAINER_heap_remove_node (struct GNUNET_CONTAINER_Heap *root,
+                                         void *element);
+
+/**
+ * Updates the cost of any node in the tree
+ */
+int
+GNUNET_CONTAINER_heap_update_cost (struct GNUNET_CONTAINER_Heap *root,
+                                   void *element, GNUNET_CostType new_cost);
+
+/**
+ * Random walk of the tree, returns the data stored at the next random node
+ * in the walk.  Calls callee with the data, or NULL if the tree is empty
+ * or some other problem crops up.
+ */
+void *GNUNET_CONTAINER_heap_walk_get_next (struct GNUNET_CONTAINER_Heap
+                                           *root);
+
+/*
+ * Returns the current size of the heap
+ *
+ * @param heap the heap to get the size of
+ */
+unsigned int
+GNUNET_CONTAINER_heap_get_size (struct GNUNET_CONTAINER_Heap *heap);
 
 
 #if 0                           /* keep Emacsens' auto-indent happy */
