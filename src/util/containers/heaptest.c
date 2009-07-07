6719891f824f3464a6e89a80e3cb00f169e9c1b2
nevans <nevans@140774ce-b5e7-0310-ab8b-a85725594a96>
2009-07-07 18:42:02 +0000

    fixed remove last element of heap bug
    git-svn-id: https://gnunet.org/svn/GNUnet@8631 140774ce-b5e7-0310-ab8b-a85725594a96

--- src/util/containers/heaptest.c
+++ src/util/containers/heaptest.c
@@ -104,7 +104,11 @@ main (int argc, char **argv)
 
   GNUNET_CONTAINER_heap_iterate (myHeap, iterator_callback, NULL);
   fprintf (stdout, "\n");
+
+  GNUNET_CONTAINER_heap_destroy(myHeap);
+
   return 0;
+
 }
 
 /* end of heaptest.c */
