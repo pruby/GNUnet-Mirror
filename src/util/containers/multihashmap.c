6a81d19536efe483ac15660332a73344f1290bad
grothoff <grothoff@140774ce-b5e7-0310-ab8b-a85725594a96>
2009-10-20 18:41:00 +0000

    backporting bugfix
    git-svn-id: https://gnunet.org/svn/GNUnet@9225 140774ce-b5e7-0310-ab8b-a85725594a96

--- src/util/containers/multihashmap.c
+++ src/util/containers/multihashmap.c
@@ -264,7 +264,10 @@ GNUNET_multi_hash_map_put (struct GNUNET_MultiHashMap *map,
         }
     }
   if (map->size / 3 > map->map_length / 4)
-    grow (map);
+    {
+      grow (map);
+      i = idx_of (map, key);
+    }
   e = GNUNET_malloc (sizeof (struct MapEntry));
   e->key = *key;
   e->value = value;
