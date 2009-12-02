b8ed5c40b8267b4596eed4ccf5422b3ecbdc2a57
grothoff <grothoff@140774ce-b5e7-0310-ab8b-a85725594a96>
2009-12-02 12:57:09 +0000

    noabort
    git-svn-id: https://ng.gnunet.org/svn/GNUnet@9667 140774ce-b5e7-0310-ab8b-a85725594a96

--- src/util/network/select.c
+++ src/util/network/select.c
@@ -339,8 +339,6 @@ readAndProcess (SelectHandle * sh, Session * session)
       if (GNUNET_OK != sh->mh (sh->mh_cls,
                                sh, session->sock, session->sock_ctx, pack))
         {	  
-          GNUNET_GE_BREAK(sh->ectx, 0);
-	  abort ();
           GNUNET_mutex_lock (sh->lock);
           if (session->locked == 1)
             session->locked = 0;
