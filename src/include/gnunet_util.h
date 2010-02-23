6c7dd6b1b6d5369adf0b8e07906b1db2cdda9552
grothoff <grothoff@140774ce-b5e7-0310-ab8b-a85725594a96>
2010-02-23 08:52:05 +0000

    readying for release
    git-svn-id: https://ng.gnunet.org/svn/GNUnet@10414 140774ce-b5e7-0310-ab8b-a85725594a96

--- src/include/gnunet_util.h
+++ src/include/gnunet_util.h
@@ -78,7 +78,7 @@ extern "C"
  * especially the least significant bits may change
  * frequently, even between different SVN versions.
  */
-#define GNUNET_UTIL_VERSION 0x00080101
+#define GNUNET_UTIL_VERSION 0x00080102
 
 #if 0                           /* keep Emacsens' auto-indent happy */
 {
